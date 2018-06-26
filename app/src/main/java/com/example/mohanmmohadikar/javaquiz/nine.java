f0b1a62e684adf4416d7ed339c398ac5063a4d54
mohanmmohadikar <chetandxg@gmail.com>
2018-06-26 18:52:00 +0530

    .

--- app/src/main/java/com/example/mohanmmohadikar/javaquiz/nine.java
+++ app/src/main/java/com/example/mohanmmohadikar/javaquiz/nine.java
@@ -1,13 +1,74 @@
 package com.example.mohanmmohadikar.javaquiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class nine extends AppCompatActivity {
+
+    Button b1;
+    RadioGroup rg;
+    RadioButton rb1,rb2,rb3,rb4;
+
+    String name;
+    int score=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nine);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setTitle("Question 9");
+
+
+        Intent i = getIntent();
+        score = i.getIntExtra("score", score);
+        name = i.getStringExtra("name");
+
+
+        b1 = (Button)findViewById(R.id.b1);
+        rg = (RadioGroup)findViewById(R.id.rg);
+        rb1 = (RadioButton)findViewById(R.id.rb1);
+        rb2 = (RadioButton)findViewById(R.id.rb2);
+        rb3 = (RadioButton)findViewById(R.id.rb3);
+        rb4 = (RadioButton)findViewById(R.id.rb4);
+
+
+        b1.setOnClickListener(v->{
+
+            q();
+            sendTo();
+
+        });
+
+
     }
+
+    private void sendTo() {
+
+        Intent i = new Intent(nine.this, ten.class);
+        i.putExtra("score", score);
+        i.putExtra("name", name);
+        startActivity(i);
+        finish();
+
+    }
+
+    private void q() {
+
+        if(rb4.isChecked()){
+            score++;
+        }
+
+
+    }
+
+
 }
+
--- app/src/main/java/com/example/mohanmmohadikar/javaquiz/result.java
+++ app/src/main/java/com/example/mohanmmohadikar/javaquiz/result.java
@@ -1,13 +1,39 @@
 package com.example.mohanmmohadikar.javaquiz;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class result extends AppCompatActivity {
+
+    TextView tv1,tv2;
+
+    String name;
+    int score=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+
+
+
+        Intent i = getIntent();
+        score = i.getIntExtra("score", score);
+        name = i.getStringExtra("name");
+
+
+        tv1 = (TextView)findViewById(R.id.tv1);
+        tv2 = (TextView)findViewById(R.id.tv2);
+
+        tv1.setText(name+"'s"+" score");
+        tv2.setText(score+"/10");
+
+
+
+
     }
 }
+
